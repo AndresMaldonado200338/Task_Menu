9e1afa602fdd973796c22fb28624a9aa19e57c34
Esteban-Coronado <esteban.coronado0613@gmail.com>
2023-08-23 16:59:30 -0500

    Agrego complemento clase task y funcionalidades

--- src/model/ListManagement.java
+++ src/model/ListManagement.java
@@ -1,5 +1,29 @@
 package model;
 
-public class List {
-    
+import java.util.ArrayList;
+
+public class ListManagement {
+
+    ArrayList<Task> listTaks;
+    Task tasks;
+
+    public void newTask(String nameTask) {
+        listTaks.add(new Task(false, nameTask));
+    }
+
+    public void completedTask(int number) {
+        listTaks.get(number).setCompleted(true);
+    }
+
+    public void viewTask() {
+
+    }
+
+    public void deleteTask() {
+
+    }
+
+    public void viewTaskIncomplete() {
+
+    }
 }
---
+++ src/model/Task.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Task {
+    private boolean completed;
+    private String nameTask;
+
+    public Task(boolean completed, String nameTask) {
+        this.completed = completed;
+        this.nameTask = nameTask;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+}
